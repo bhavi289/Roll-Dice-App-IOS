2b5eec8ad3aa352092ce9f2d88a2e8ccd1c5a975
bhavi289 <bhavi.c16@iiits.in>
2018-08-13 21:16:56 +0530

    app load dice faces and functions

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,15 +30,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-//        diceImageView1.image = dice-2
         diceImageView1.image = UIImage(named: dicearray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: dicearray[ ])
-        
-        print(randomDiceIndex1)
+        diceImageView2.image = UIImage(named: dicearray[randomDiceIndex2])
     }
-    
 }
 
