ef8865bb021a6926900baca864969e933d809efe
bhavi289 <bhavi.c16@iiits.in>
2018-08-13 21:04:55 +0530

    random dice images on button click

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0;
     var randomDiceIndex2 : Int = 0;
     
+    let dicearray = ["dice-1","dice-2","dice-3","dice-4","dice-5","dice-6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -29,6 +31,10 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+//        diceImageView1.image = dice-2
+        diceImageView1.image = UIImage(named: dicearray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: dicearray[ ])
         
         print(randomDiceIndex1)
     }
